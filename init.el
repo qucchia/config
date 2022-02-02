fcd9f427eeea09cc7f93dd7e96dba24c90155ec9
qucchia <yijods@gmail.com>
2022-02-02 13:05:06 +0100

    Add LSP mode

--- init.el
+++ init.el
@@ -391,6 +391,20 @@
 
 (use-package json-mode)
 
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap
+  (setq lsp-keymap-prefix "C-c l")
+  :hook
+  (typescript-mode lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
 ;; Start server to allow opening files from other applications
 (server-start)
 
