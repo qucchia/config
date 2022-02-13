9c2ef0d474a90d894bfe95f5ccaad80bb703ea59
qucchia <qucchia0@gmail.com>
2022-02-13 20:00:36 +0100

    Fix some setup issues

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-exec dbus-launch --exit-with-session emacs -q -l ~/Projects/config/emacs/init.el -mm --debug-init
+xkbcomp ~/Documents/config/layout/.Xkeymap $DISPLAY
+exec dbus-launch --exit-with-session emacs -mm --debug-init
