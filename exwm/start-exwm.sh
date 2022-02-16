e86df6e13727236441ad5d007f9bfcdc72045b09
qucchia <qucchia0@gmail.com>
2022-02-16 20:07:35 +0100

    Move desktop configuration to its own file

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 xkbcomp ~/Documents/config/layout/.Xkeymap $DISPLAY
-exec dbus-launch --exit-with-session emacs -mm --debug-init
+exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/Documents/config/exwm/desktop.el
