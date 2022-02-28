6af6d1ccb5b4259934bc87f4bb70da6bbc4abf80
qucchia <qucchia0@gmail.com>
2022-02-28 15:24:21 +0100

    Improve EXWM start script

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-xkbcomp ~/Documents/config/layout/.Xkeymap $DISPLAY
-exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/Documents/config/exwm/desktop.el
+export CONFIG_DIR=$HOME/Documents/config
+
+# Set keyboard layout
+xmodmap $CONFIG_DIR/layout/.Xmodmap
+
+# Start Tor service
+sudo systemctl start tor@default.service
+
+# Start Emacs
+exec dbus-launch --exit-with-session emacs -mm --debug-init -l $CONFIG_DIR/exwm/desktop.el
