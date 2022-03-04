170f294e7f12c7662aa3d87f3914448e92868a0e
qucchia <qucchia0@gmail.com>
2022-03-04 15:35:13 +0100

    Separate Window Manager into sections

--- exwm/desktop.el
+++ exwm/desktop.el
@@ -20,36 +20,37 @@
   (require 'exwm-systemtray)
   (exwm-systemtray-enable)
 
+  
   (setq exwm-input-prefix-keys
-     '(?\C-x
-       ?\C-u
-       ?\C-h
-       ?\M-x
-       ?\M-`
-       ?\M-&
-       ?\M-:
-       ?\C-\M-j
-       ?\C-\ )) ;; C-SPC
-
+        '(?\C-x
+          ?\C-u
+          ?\C-h
+          ?\M-x
+          ?\M-`
+          ?\M-&
+          ?\M-:
+          ?\C-\M-j
+          ?\C-\ )) ;; C-SPC
+  
   ;; C-q will always send the next key directly
   (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
-
+  
   (setq exwm-input-global-keys
-      `(
-           ;; Reset to line-mode
+        `(
+          ;; Reset to line-mode
           ([?\s-r] . exwm-reset)
-
+  
           ;; Move between windows
           ([?\s-h] . windmove-left)
           ([?\s-j] . windmove-down)
           ([?\s-k] . windmove-up)
           ([?\s-l] . windmove-right)
-
+  
           ;; Launch applications with shell command
           ([?\s-&] . (lambda (command)
                        (interactive (list (read-shell-command "$ ")))
                        (start-process-shell-command command nil command)))
-
+  
           ;; Switch workspace
           ([?\s-w] . exwm-workspace-switch)
           ,@(mapcar (lambda (i)
@@ -58,17 +59,21 @@
                           (interactive)
                           (exwm-workspace-switch-create ,i))))
                     (number-sequence 0 9))))
+  
 
+  
   ;; Show time
   (setq display-time-default-load-average nil)
   (setq display-time-24hr-format t)
   (display-time-mode t)
-
+  
   ;; Start server to allow opening files from other applications
   (server-start)
-
+  
   (exwm-enable)
-
+  
+  ;; Play Code Radio
   (emms-play-url "https://coderadio-relay-ffm.freecodecamp.org/radio/8010/radio.mp3")
-
-  (eshell-command "ssh-add ~/.ssh/codeberg"))
+  
+  (eshell-command "ssh-add ~/.ssh/codeberg")
+  )
