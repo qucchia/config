74258f3c1e6330e2207c001aba6e10d40e932bee
qucchia <qucchia0@gmail.com>
2022-03-09 15:38:47 +0100

    Add qbangle repository

--- setup/setup.sh
+++ setup/setup.sh
@@ -37,6 +37,7 @@ mkdir -p ~/Projects ~/Mail
 git clone git@codeberg.org:qucchia/life ~/Documents/life
 git clone git@codeberg.org:qucchia/school ~/Documents/school
 git clone git@codeberg.org:qucchia/mafia-bot ~/Projects/mafia-bot
+git clone git@codeberg.org:qbangle/qbangle ~/Projects/qbangle
 
 mkdir -p ~/.local/share/fonts
 cd /tmp
