1ddff6a3efe52b9cdafefc95eb1ba592fe67094c
qucchia <qucchia0@gmail.com>
2022-03-14 19:32:37 +0100

    Configure dired

--- emacs/init.el
+++ emacs/init.el
@@ -565,6 +565,34 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :bind (("C-x C-j" . dired-jump))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-single-up-directory
+    "l" 'dired-single-buffer))
+
+(use-package dired-single)
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired-open
+  :config
+  (setq dired-open-extensions '(("png" . "display"))))
+
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "H" 'dired-hide-dotfiles-mode))
+
+(use-package diredfl
+  :hook (dired-mode . diredfl-mode))
+
 (use-package term
   :config
   (setq explicit-shell-file-name "bash")
