1288de3d7e6c64dc59da752a0ce2f9c2b80a85ea
qucchia <qucchia0@gmail.com>
2022-04-02 11:22:31 +0200

    Fix some formatting

--- emacs/init.el
+++ emacs/init.el
@@ -1,9 +1,9 @@
 (defun qucchia/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                     (time-subtract after-init-time before-init-time)))
-           gcs-done))
+    (format "%.2f seconds"
+    (float-time
+      (time-subtract after-init-time before-init-time)))
+  gcs-done))
 
 (add-hook 'emacs-startup-hook #'qucchia/display-startup-time)
 
