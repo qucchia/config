8857361d55999077a2d68aaaca5c29a691b44856
qucchia <qucchia0@gmail.com>
2022-04-05 16:21:39 +0200

    Add keyboard and locale options

---
+++ root/etc/profile.d/proxy.sh
@@ -0,0 +1,9 @@
+#export http_proxy="http://192.168.1.38:9050"
+#export https_proxy="http://192.168.1.38:9050"
+#export ftp_proxy="http://192.168.1.38:9050"
+#export no_proxy="127.0.0.1,localhost"
+
+#export HTTP_PROXY="http://192.168.1.38:9050"
+#export HTTPS_PROXY="http://192.168.1.38:9050"
+#export FTP_PROXY="http://192.168.1.38:9050"
+#export NO_PROXY="127.0.0.1,localhost"
