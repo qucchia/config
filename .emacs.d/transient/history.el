448bec0411c81d02384a6d5ab1ede1a9862bbf46
qucchia <qucchia0@gmail.com>
2022-04-05 16:22:50 +0200

    Remove transient/history.el from source control

--- .emacs.d/transient/history.el
+++
@@ -1,8 +0,0 @@
-((magit-commit nil)
- (magit-dispatch nil)
- (magit-merge nil)
- (magit-push nil)
- (magit-remote
-  ("-f"))
- (magit-remote\.<remote>\.*url "git@github.com:qucchia/scratch-gui" "https://github.com/TurboWarp/scratch-gui")
- (magit-reset nil))
