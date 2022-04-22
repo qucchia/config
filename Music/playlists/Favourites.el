cd9393c725130034f215cc8e9320ee2ec1a326f4
qucchia <qucchia0@gmail.com>
2022-04-22 07:15:48 +0200

    Use elisp to store playlist instead of m3u

---
+++ Music/playlists/Favourites.el
@@ -0,0 +1,17 @@
+(setq racket-favourites-playlist
+  '(("Star Wars"
+      . (("The Mandalorian"
+            . (("Chapter 01" . ("07 Bounty Droid"))
+               ("Chapter 05" . ("06 Night Riders"))
+							 ("Chapter 06" . ("02 The Gang"
+																"05 Hyperspace"
+																"06 Little Mousey"
+																"08 My Saviour"
+																"11 Mando's Back"))
+							 ("Chapter 07" . ("01 Man of Honour"))))
+				 ("04 A New Hope" . ("08 - Inner City"
+														 "09 - Cantina Band"))
+				 ("05 The Empire Strikes Back" . ("07 - Han Solo and the Princess"
+																					"10 - The Asteroid Field"
+																					"17 - Finale"))))
+ ))
