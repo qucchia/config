cccd3a5aad4f6dbcce26cff1b1f468acd302336d
qucchia <qucchia0@gmail.com>
2022-04-27 17:54:36 +0200

    Update favourites playlist

---
+++ home/Music/playlists/favourites.el
@@ -0,0 +1,42 @@
+(setq
+ racket-playlist
+ '(("Star Wars"
+    . (("01 The Phantom Menace" . ("01 - Star Wars Main Title and the Arrival at Naboo"
+                                   "05 - The Sith Spacecraft and the Droid Battle"
+                                   "14 - The Droid Invasion and the Appearance of Darth Maul"))
+       ("02 Attack of the Clones" . ("01 - Star Wars Main Title and Ambush on Coruscant"
+                                     "03 - Zam the Assassin and the Chase Through Coruscant"))
+       ("03 Revenge of the Sith" . ("01 - Star Wars and the Revenge of the Sith"
+                                    "10 - Anakin's Dark Deeds"))
+       ("04 A New Hope" . ("08 - Inner City"
+                           "09 - Cantina Band"))
+       ("05 The Empire Strikes Back" . ("07 - Han Solo and the Princess"
+                                        "10 - The Asteroid Field"
+                                        "17 - Finale"))
+       ("The Mandalorian"
+        . (("Chapter 01" . ("05 Burg Attack"
+                            "07 Bounty Droid"))
+           ("Chapter 02" . ("02 Jawas Attack"
+                            "06 The Mudhorn"))
+           ("Chapter 03" . ("03 Signet Forging"
+                            "06 Mando Rescue"))
+           ("Chapter 04" . ("01 The Ponds of Sorgan"
+                            "04 Training the Plebs"))
+           ("Chapter 05" . ("04 Speederbikes"
+                            "06 Night Riders"))
+           ("Chapter 06" . ("02 The Gang"
+                            "05 Hyperspace"
+                            "06 Little Mousey"
+                            "08 My Saviour"
+                            "10 Nice Family"
+                            "11 Mando's Back"))
+           ("Chapter 07" . ("01 Man of Honour"
+                            "02 Reprogram"
+                            "04 The Standoff"
+                            "07 The Arrival"))
+           ("Chapter 08" . ("01 Check Point"
+                            "02 Nurse Droid"
+                            "05 Nurse and Protect"
+                            "08 Clan of Two"))
+           ("Chapter 09" . ("02 Enjoy the Fights"))))))
+ ))
